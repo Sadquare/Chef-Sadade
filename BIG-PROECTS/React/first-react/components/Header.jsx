d95b5dc4d62096e0d59f8497d53fde10e09d71c6
unknown <idrissasadade0205@gmail.com>
2025-04-13 18:32:51 +0100

    Supression images inutiles

--- BIG-PROECTS/React/first-react/components/Header.jsx
+++ BIG-PROECTS/React/first-react/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header(){
     return(
         <header>
             <img  src="../src/assets/s1.png"/>
-            <h2>👨‍🍳 Chef Claude </h2>
+            <h2>👨‍🍳 Chef Sadade </h2>
         </header>
     )
 }
