ec74ad00ed049ee0f6460109f870485078375010
unknown <idrissasadade0205@gmail.com>
2025-04-13 19:45:09 +0100

    image loading

--- BIG-PROECTS/React/first-react/components/Header.jsx
+++ BIG-PROECTS/React/first-react/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header(){
     return(
         <header>
-            <img  src="../src/assets/s1.png"/>
+            <img src="/s1.png" alt="Logo" />
             <h2>👨‍🍳 Chef Sadade </h2>
         </header>
     )
